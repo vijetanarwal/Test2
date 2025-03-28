e116762ae8978224552781049a02a81c601ceabf
Mahima <mahima@classplus.co>
2025-03-28 11:40:17 +0530

    added some work

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -2,6 +2,6 @@ package com.example;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello, Jenkins!");
+        System.out.println("Print World!");
     }
 }
